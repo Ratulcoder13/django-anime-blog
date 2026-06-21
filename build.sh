72d227ff7006c609f142768abec6bb12956bc3f9
Ratulcoder13 <ratulmdmarufhossain@gmail.com>
2026-06-21 20:03:23 +0600

    removed migrate from build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # প্রয়োজনীয় প্যাকেজ ইনস্টল এবং স্ট্যাটিক ফাইল কালেকশন
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate
+
 export DJANGO_SETTINGS_MODULE="myapp.settings"
 # 🎯 এই কোডটুকু অটোমেটিক অনলাইনে আপনার এডমিন অ্যাকাউন্ট বানিয়ে দেবে
 python -c "
